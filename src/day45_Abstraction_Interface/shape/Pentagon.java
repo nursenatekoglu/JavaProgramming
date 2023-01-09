7cc60b6178d59635b2a7b5225ca7146b926d9a44
Nursena Tekoglu <nursena.tekoglu@gmail.com>
2023-01-09 21:45:36 +0300

    day 45 Abstraction

---
+++ src/day45_Abstraction_Interface/shape/Pentagon.java
@@ -0,0 +1,17 @@
+package day45_Abstraction_Interface.shape;
+
+public class Pentagon extends Shape{
+    public Pentagon(String name) {
+        super(name);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+}
